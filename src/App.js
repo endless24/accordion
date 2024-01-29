1316a173082c94a01445ca12d91d9127695b8392
endless24 <jcchi2004@yahoo.com>
2024-01-29 16:41:35 +0100

    throught with it

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./index.css";
 
 const faqs = [
@@ -16,7 +17,53 @@ const faqs = [
 ];
 
 function App() {
-  return <div className="">kmnlk</div>;
+  return (
+    <div className="">
+      <Accordion data={faqs} />
+    </div>
+  );
 }
+
+const Accordion = ({ data }) => {
+  const [isOpen, setIsOpen] = useState(null);
+
+  return (
+    <div className="accordion">
+      {data.map((el, index) => (
+        <AccordionItems
+          title={el.title}
+          num={index}
+          key={el.title}
+          isOpen={isOpen}
+          onOpen={setIsOpen}
+        >
+          {el.text}
+        </AccordionItems>
+      ))}
+    </div>
+  );
+};
+
+const AccordionItems = ({ num, title, isOpen, onOpen, children }) => {
+  // checking if
+  const curOpen = num === isOpen;
+  //calling the function
+  const handleToggle = () => {
+    //calling function if it close onclick open or close
+    onOpen(curOpen ? null : num);
+  };
+
+  return (
+    <div className={`item ${curOpen ? "open" : ""}`} onClick={handleToggle}>
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>{" "}
+      {/* formatting the number*/}
+      <p className="title">{title}</p>
+      <p className="icon">{curOpen ? "-" : "+"}</p>
+      {/* adding the plus and minus*/}
+      {curOpen && <p className="content-box">{children}</p>}
+      {/* hiddening the content*/}
+    </div>
+  );
+};
 
 export default App;
